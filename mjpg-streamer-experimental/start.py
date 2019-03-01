6694d9ac6d1d56f15d1d9cf9bfa0eec9a9c9d438
Nathan Webb <nathanwebb7550@gmail.com>
2019-02-28 18:37:40 -0600

    Add files via upload

--- mjpg-streamer-experimental/start.py
+++ mjpg-streamer-experimental/start.py
@@ -2,16 +2,33 @@ import os
 import fnmatch
 import time
 import multiprocessing
+import re
+
+pattern = re.compile("MODEL_ID")
 
 cameras = []
 
 for file in os.listdir('/dev'): # For each plugged in camera
   if fnmatch.fnmatch(file, 'video[0-9]'):
       cameras.append(int(file[-1:])) # Add camera ID to list
+print cameras
 
 def launchcam(id, port):
-    os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(port)+'" -i "input_opencv.so --filter cvfilter_py.so --fargs ./compression.py --d '+str(cameras[id])+'"')
+    os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(port)+'" -i "./input_opencv.so --filter ./cvfilter_py.so --fargs ./compression.py --d '+str(cameras[id])+'"')
 
+'''
+def back_camera():
+    for camera in cameras:
+	for i, line in enumerate(os.popen("sudo udevadm info --query=all /dev/video"+str(camera))):
+	    if "E: ID_MODEL_ID=" in str(line) and ( not "0779" in str(line) ):
+                os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(25565)+'" -i "input_opencv.so --d '+str(camera)+'"')
+def front_camera():
+    for camera in cameras:
+	for i, line in enumerate(os.popen("sudo udevadm info --query=all /dev/video"+str(camera))):
+	    if "E: ID_MODEL_ID=0779" in str(line):
+                os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(25567)+'" -i "input_opencv.so --d '+str(camera)+'"') 
+'''
+	
 p1 = multiprocessing.Process(target=launchcam,args=(0,25565,))
 p1.start()
 time.sleep(2)
@@ -20,4 +37,6 @@ p2.start()
 time.sleep(2)
 p1.join()
 
-#os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(25567)+'" -i "input_opencv.so --d '+str(cameras[2])+'"')
+
+
+
