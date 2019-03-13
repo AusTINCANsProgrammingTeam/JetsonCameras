0c6c4eafb7fb723c3e2d568b7e9586359d65a82c
Jack <jack.latham.lewis@gmail.com>
2019-03-12 19:08:32 -0500

    one camera works now

--- mjpg-streamer-experimental/start.py
+++ mjpg-streamer-experimental/start.py
@@ -11,7 +11,6 @@ cameras = []
 for file in os.listdir('/dev'): # For each plugged in camera
   if fnmatch.fnmatch(file, 'video[0-9]'):
       cameras.append(int(file[-1:])) # Add camera ID to list
-print cameras
 
 def launchcam(id, port):
     os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(port)+'" -i "./input_opencv.so --filter ./cvfilter_py.so --fargs ./compression.py --d '+str(cameras[id])+'"')
@@ -27,14 +26,14 @@ def front_camera():
 	for i, line in enumerate(os.popen("sudo udevadm info --query=all /dev/video"+str(camera))):
 	    if "E: ID_MODEL_ID=0779" in str(line):
                 os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(25567)+'" -i "input_opencv.so --d '+str(camera)+'"') 
-'''
-	
+'''	
 p1 = multiprocessing.Process(target=launchcam,args=(0,80,))
 p1.start()
 time.sleep(2)
-p2 = multiprocessing.Process(target=launchcam,args=(1,441,))
-p2.start()
-time.sleep(2)
+if len(cameras) > 1:
+	p2 = multiprocessing.Process(target=launchcam,args=(1,441,))
+	p2.start()
+	time.sleep(2)
 p1.join()
 
 
