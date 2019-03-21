e0eef505ff2537e8aa514393a78156b44cb61ad1
Jack <jack.latham.lewis@gmail.com>
2019-03-20 20:55:04 -0500

    all we need now is the device ids

--- mjpg-streamer-experimental/start.py
+++ mjpg-streamer-experimental/start.py
@@ -8,10 +8,21 @@ pattern = re.compile("MODEL_ID")
 
 cameras = []
 
+visionCamID = "IDHERE"
+cameraNum = None
+cameraMap = open("/home/nvidia/cameraMap.txt", "r")
+for cameraStr in cameraMap:
+  if visionCamID in cameraStr:
+    cameraNum = cameraStr.split()[0]
+    cameraNum = cameraNum.replace("/dev/video", "")
+    cameraNum = int(cameraNum)
+    cameras.append(cameraNum)
+'''
 for file in os.listdir('/dev'): # For each plugged in camera
   if fnmatch.fnmatch(file, 'video[0-9]'):
       cameras.append(int(file[-1:])) # Add camera ID to list
 #print cameras
+'''
 
 def launchcam(id, port):
     os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(port)+'" -i "./input_opencv.so --filter ./cvfilter_py.so --fargs ./compression.py --d '+str(cameras[id])+'"')
@@ -29,10 +40,10 @@ def front_camera():
                 os.popen('mjpg_streamer -o "output_http.so -w ./www -p '+str(25567)+'" -i "input_opencv.so --d '+str(camera)+'"') 
 '''
 	
-p1 = multiprocessing.Process(target=launchcam,args=(0,5800,))
+p1 = multiprocessing.Process(target=launchcam,args=(cameras[0],5800,))
 p1.start()
 time.sleep(2)
-p2 = multiprocessing.Process(target=launchcam,args=(1,5801,))
+p2 = multiprocessing.Process(target=launchcam,args=(cameras[1],5801,))
 p2.start()
 time.sleep(2)
 p1.join()
